4e375c084a565d67952e78c505cf63cd91c38d51
nagallegos <113269909+nagallegos@users.noreply.github.com>
2023-05-23 11:54:04 -0500

    Finished the youtube video

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,26 @@
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
-  // or <Message></Message>
-  // Below is "self closing" syntax
-  // which is more concise.
-  return <div><ListGroup /></div>;
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  const handleClick = () => {
+    console.log(alertVisible);
+    setAlertVisibility(true);
+  };
+
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button color="primary" onClick={handleClick}>
+        My Button
+      </Button>
+    </div>
+  );
 }
 
 // Exporting so that the component can
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,16 +1,55 @@
 //import { Fragment } from "react";
+//import { MouseEvent } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
-  items = [];
+import { useState } from "react";
+
+// { items: [], heading: string }
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  //items = [];
+
+  //let selectedIndex = 0;
+
+  // Hook
+  //const arr = useState(-1);
+  //arr[0]; // variable (selectedIndex)
+  //arr[1]; // updater function
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  //const message = items.length === 0 ? <p>No item found</p> : null;
+  //{items.length === 0 && <p>No item found</p>} // Using logical and to perform the same task.
+  const getMessage = () => {
+    return items.length === 0 ? <p>No item found</p> : null;
+  };
+
+  // Event Handler
+  //const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
-      <h1>List</h1>
-      {items.length === 0 ? <p>No item found</p> : null}
+      <h1>{heading}</h1>
+      {getMessage()}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
